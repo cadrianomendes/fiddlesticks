f1d4f48e0666fc723e48f5b5a6ea480d200a5d33
Adriano <c.adrianomendes@gmail.com>
2015-10-16 10:46:25 -0300

    third

--- src/test/java/gg/bozoapps/printer/Application.java
+++ src/test/java/gg/bozoapps/printer/Application.java
@@ -18,11 +18,11 @@ public class Application {
             }
         };
     }
-
+/*
   public static void main(String[] args) {
       ApplicationContext context = 
           new AnnotationConfigApplicationContext(Application.class);
       MessagePrinter printer = context.getBean(MessagePrinter.class);
       printer.printMessage();
-  }
+  }*/
 }
